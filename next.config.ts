2f5b18ba9c610b481b44f04ac2cb781dbd0ba3c5
Vishavjeet Singh <vishavjeetsinghthakur@gmail.com>
2025-08-10 17:43:02 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- next.config.ts
+++ next.config.ts
@@ -18,20 +18,6 @@ const nextConfig: NextConfig = {
       },
     ],
   },
-  webpack: (
-    config,
-    { isServer }
-  ) => {
-    // Exclude async_hooks from client-side builds
-    if (!isServer) {
-      config.resolve.fallback = {
-        ...config.resolve.fallback,
-        async_hooks: false,
-      };
-    }
-
-    return config;
-  },
 };
 
 export default nextConfig;
